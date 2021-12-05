4b025668c7c31002b7758191760120aec09bd0d4
kth <kth09230923@gmail.com>
2021-12-05 13:09:59 +0900

    Dom 이용

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,25 @@
 "use strict"
 
-console.log("test!")
+console.log("Login JS");
+
+const id = document.querySelector("#id"),
+pw = document.querySelector("#pw"),
+loginBtn = document.querySelector("Button")
+
+console.log(id)
+console.log(pw)
+console.log(loginBtn)
+
+loginBtn.addEventListener("click" , login);
+
+function login(){
+    console.log("login start")
+
+    const req = {
+        id : id.value,
+        pw : pw.value
+    }
+
+    console.log(req)
+
+}
